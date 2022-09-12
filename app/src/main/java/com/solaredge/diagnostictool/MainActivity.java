51e0916aa05495acf4e037cf158a84f26e047e50
eyren24 <retyma24@gmail.com>
2022-09-12 15:44:59 +0200

    change bottom bar app

--- app/src/main/java/com/solaredge/diagnostictool/MainActivity.java
+++ app/src/main/java/com/solaredge/diagnostictool/MainActivity.java
@@ -15,21 +15,21 @@ import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomAppBar mBottomAppBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomAppBar bottomAppBar = findViewById(R.id.bottomAppBar);
-        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+        mBottomAppBar = findViewById(R.id.bottomAppBar);
+        mBottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
                 switch (item.getItemId()){
-                    case R.id.page_1:
-                        Toast.makeText(getApplicationContext(), "page1", Toast.LENGTH_SHORT).show();
-                    case R.id.page_2:
+                    case R.id.camera:
                         switchActivities();
+                        break;
                     default:
-                        Toast.makeText(getApplicationContext(), "Select someone", Toast.LENGTH_SHORT).show();
+                        break;
                 }
                 return false;
             }
