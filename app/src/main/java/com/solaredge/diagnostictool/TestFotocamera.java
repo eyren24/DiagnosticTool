3c7635a042075ebbb46173c50bb7b7bfcde522cc
eyren24 <retyma24@gmail.com>
2022-09-12 16:15:59 +0200

    Finished bottom app bar fotocamera app

--- app/src/main/java/com/solaredge/diagnostictool/TestFotocamera.java
+++ app/src/main/java/com/solaredge/diagnostictool/TestFotocamera.java
@@ -2,17 +2,38 @@ package com.solaredge.diagnostictool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class TestFotocamera extends AppCompatActivity {
+    private BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_fotocamera);
+
+
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navListener);
     }
 
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener = item -> {
+        int itemId = item.getItemId();
+        if (itemId == R.id.home) {
+            return true;
+        } else if (itemId == R.id.camera) {
+            switchActivities();
+        }
+        return true;
+    };
 
+    private void switchActivities() {
+        Intent switchActivityIntent = new Intent(this, MainActivity.class);
+        startActivity(switchActivityIntent);
+    }
 
 }
 
