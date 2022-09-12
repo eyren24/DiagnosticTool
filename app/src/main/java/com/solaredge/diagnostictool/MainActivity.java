43ae801c25803b21883ce27d25fdc84544475d93
eyren24 <retyma24@gmail.com>
2022-09-12 14:39:33 +0200

    creating bottom app bar

--- app/src/main/java/com/solaredge/diagnostictool/MainActivity.java
+++ app/src/main/java/com/solaredge/diagnostictool/MainActivity.java
@@ -2,15 +2,21 @@ package com.solaredge.diagnostictool;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomAppBar bottomAppBar=findViewById(R.id.bottom_navigation) ;
     }
-
-
 }
