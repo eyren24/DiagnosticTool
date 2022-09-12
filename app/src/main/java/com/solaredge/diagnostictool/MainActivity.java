edfe96b5a67597067420acdd383be3e83aa492af
eyren24 <retyma24@gmail.com>
2022-09-12 16:12:22 +0200

    Finished bottom app bar

--- app/src/main/java/com/solaredge/diagnostictool/MainActivity.java
+++ app/src/main/java/com/solaredge/diagnostictool/MainActivity.java
@@ -3,38 +3,35 @@ package com.solaredge.diagnostictool;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    private BottomAppBar mBottomAppBar;
+    private BottomNavigationView bottomNavigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mBottomAppBar = findViewById(R.id.bottomAppBar);
-        mBottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.camera:
-                        switchActivities();
-                        break;
-                    default:
-                        break;
-                }
-                return false;
-            }
-        });
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navListener);
+
     }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener = item -> {
+        int itemId = item.getItemId();
+        if (itemId == R.id.home) {
+            return true;
+        } else if (itemId == R.id.camera) {
+            switchActivities();
+        }
+        return true;
+    };
+
     private void switchActivities() {
         Intent switchActivityIntent = new Intent(this, TestFotocamera.class);
         startActivity(switchActivityIntent);
