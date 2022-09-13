c2d044a46ce655ef638d936c48e4bf1e9a50adb2
eyren24 <retyma24@gmail.com>
2022-09-13 09:28:05 +0200

    remove bottom nav bar

--- app/src/main/java/com/solaredge/diagnostictool/MainActivity.java
+++ app/src/main/java/com/solaredge/diagnostictool/MainActivity.java
@@ -13,30 +13,19 @@ import androidx.fragment.app.Fragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    private BottomNavigationView bottomNavigation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bottomNavigation = findViewById(R.id.bottom_navigation);
-        bottomNavigation.setOnNavigationItemSelectedListener(navListener);
     }
+
     protected void onResume(){
         super.onResume();
         VideoView videoView = (VideoView) findViewById(R.id.videoView);  //casting to VideoView is not Strictly required above API level 26
         videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.bg); //set the path of the video that we need to use in our VideoView
         videoView.start();  //start() method of the VideoView class will start the video to play
     }
-
-    private final BottomNavigationView.OnNavigationItemSelectedListener navListener = item -> {
-        int itemId = item.getItemId();
-        if (itemId == R.id.home) {
-            return true;
-        } else if (itemId == R.id.camera) {
-            switchActivities();
-        }
-        return true;
-    };
 
     private void switchActivities() {
         Intent switchActivityIntent = new Intent(this, TestFotocamera.class);
