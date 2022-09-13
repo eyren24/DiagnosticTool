e95f75740743674a634554428c2b4fb7e03aa737
eyren24 <retyma24@gmail.com>
2022-09-13 09:09:29 +0200

    camera working

--- app/src/main/java/com/solaredge/diagnostictool/TestFotocamera.java
+++ app/src/main/java/com/solaredge/diagnostictool/TestFotocamera.java
@@ -1,23 +1,115 @@
 package com.solaredge.diagnostictool;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
 import android.os.Bundle;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 public class TestFotocamera extends AppCompatActivity {
-    private BottomNavigationView bottomNavigation;
+
+    private CameraCaptureSession myCameraCaptureSession;
+    private String myCameraID;
+    private CameraManager myCameraManagaer;
+    private CameraDevice myCameraDevice;
+    private TextureView myTexureView;
+    private CaptureRequest.Builder myCaptureRequestBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_fotocamera);
+        myTexureView = findViewById(R.id.textureView);
+        myCameraManagaer = (CameraManager) getSystemService(CAMERA_SERVICE);
+        openCamera();
+    }
 
+    private CameraDevice.StateCallback myStateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            myCameraDevice = camera;
+        }
 
-        bottomNavigation = findViewById(R.id.bottom_navigation);
-        bottomNavigation.setOnNavigationItemSelectedListener(navListener);
+        @Override
+        public void onDisconnected(@NonNull CameraDevice cameraDevice) {
+            myCameraDevice.close();
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice cameraDevice, int error) {
+            myCameraDevice.close();
+            myCameraDevice = null;
+        }
+    };
+
+    private void openCamera() {
+        try {
+            myCameraID = myCameraManagaer.getCameraIdList()[0];
+            ActivityCompat.requestPermissions(TestFotocamera.this, new String[] {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            myCameraManagaer.openCamera(myCameraID, myStateCallback, null);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void cameraPreview(View view){
+        SurfaceTexture mySurfaceTexure = myTexureView.getSurfaceTexture();
+        Surface surface = new Surface(mySurfaceTexure);
+        try {
+            myCaptureRequestBuilder = myCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            myCaptureRequestBuilder.addTarget(surface);
+            myCameraDevice.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(@NonNull CameraCaptureSession session) {
+                    myCameraCaptureSession = session;
+                    myCaptureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+
+                    try {
+                        myCameraCaptureSession.setRepeatingRequest(myCaptureRequestBuilder.build(),null, null);
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+
+                }
+            }, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
     }
 
     private final BottomNavigationView.OnNavigationItemSelectedListener navListener = item -> {
