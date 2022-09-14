65aa9ed29d2c9365bababbfafe6e8e377b2b473c
eyren24 <retyma24@gmail.com>
2022-09-14 16:41:05 +0200

    - Power Button check created

---
+++ app/src/main/java/com/solaredge/diagnostictool/TestPowerButton.java
@@ -0,0 +1,14 @@
+package com.solaredge.diagnostictool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestPowerButton extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_power_button);
+    }
+}
