6fad62ef243b02fd9a795d39c0d6b9142d9f1887
eyren24 <retyma24@gmail.com>
2022-09-14 12:26:22 +0200

    - Fingerprint style renew

--- app/src/main/java/com/solaredge/diagnostictool/TestFingerPrint.java
+++ app/src/main/java/com/solaredge/diagnostictool/TestFingerPrint.java
@@ -2,11 +2,13 @@ package com.solaredge.diagnostictool;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.biometric.BiometricPrompt;
+import androidx.compose.ui.text.input.TextFieldValue;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.ContextCompat;
 
@@ -20,11 +22,13 @@ public class TestFingerPrint extends AppCompatActivity {
     protected Executor executor;
     protected BiometricPrompt.PromptInfo promptInfo;
     private ConstraintLayout constraintLayout;
+    private TextView fingerprintalert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_finger_print);
+        fingerprintalert = findViewById(R.id.fingerprintalert);
 
         //Init
         executor = ContextCompat.getMainExecutor(this);
@@ -32,18 +36,21 @@ public class TestFingerPrint extends AppCompatActivity {
             @Override
             public void onAuthenticationError(int errorCode, @NotNull CharSequence errString) {
                 super.onAuthenticationError(errorCode, errString);
+                fingerprintalert.setText("Authentication error: " + errString + "\n error code: " + errorCode);
                 Toast.makeText(getApplicationContext(), "Authentication error: " + errString + "\n error code: " + errorCode, Toast.LENGTH_LONG).show();
             }
 
             @Override
             public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
                 super.onAuthenticationSucceeded(result);
+                fingerprintalert.setText("Authentication succeeded");
                 Toast.makeText(getApplicationContext(), "Authentication succeeded", Toast.LENGTH_LONG).show();
             }
 
             @Override
             public void onAuthenticationFailed() {
                 super.onAuthenticationFailed();
+                fingerprintalert.setText("Authentication failed");
                 Toast.makeText(getApplicationContext(), "Authentication failed!", Toast.LENGTH_LONG).show();
             }
 
